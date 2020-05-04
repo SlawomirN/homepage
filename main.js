086f09138922cd348eb6286d9b00bb8c400e04c0
Slawek Nowodworski <s.nowodworski@gmail.com>
2020-05-04 16:32:34 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+alert('Witaj na mojej stronie, zajrzyj do consoli po więcej informacji');
+const firstName = 'Sławek';
+const age = 100;
+console.log(`Siema, nazywam się ${firstName}, zajrzyj  i mam ${age} lat.`);
