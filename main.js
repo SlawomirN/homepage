51a74df045cd7281974d71892f8986d537b05e85
Slawek Nowodworski <s.nowodworski@gmail.com>
2020-05-17 07:30:07 +0200

    Week 4 homework done - BEM, JS in DOM

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-alert('Witaj na mojej stronie, zajrzyj do consoli po więcej informacji');
-const firstName = 'Sławek';
-const age = 200;
-console.log(`Siema, nazywam się ${firstName}, zajrzyj  i mam ${age} lat.`);
+// alert('Witaj na mojej stronie, zajrzyj do consoli po więcej informacji');
+// const firstName = 'Sławek';
+// const age = 200;
+// console.log(`Siema, nazywam się ${firstName}, zajrzyj  i mam ${age} lat.`);
+
+// const header = document.getElementsByClassName('.main-header');
+// console.log(header);
+
+const header = document.querySelector('.main-header--js');
+console.log(header);
+header.innerHTML = 'Witaj w moim dzienniczku!';
+
+const footer = document.querySelector('.footer--js');
+footer.innerHTML = '<strong>&#169; Strona stworzona w ramach szkolenia WTF</strong>';
