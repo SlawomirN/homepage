52dadae9f485d63a397f65e55a12f5e80537b6bf
Slawek Nowodworski <s.nowodworski@gmail.com>
2020-05-27 11:13:19 +0200

    changing display using event listener function in button

--- main.js
+++ main.js
@@ -7,9 +7,10 @@ console.log(`Siema, nazywam się ${firstName}, zajrzyj  i mam ${age} lat.`);
 // console.log(header); 
 // powyżej - jQuery
 
-const header = document.querySelector('.main-header--js');
-console.log(header);
-header.innerHTML = 'Witaj w moim dzienniczku!';
+
+// const header = document.querySelector('.main-header--js');
+// console.log(header);
+// header.innerHTML = 'Witaj w moim dzienniczku!';
 
 
 
@@ -25,7 +26,7 @@ header.innerHTML = 'Witaj w moim dzienniczku!';
 
 function greetOld(age, firstName) {
     console.log(
-        'Witaj Drogi Odwiedzający, nazywam się ' +  firstName + ' mam ' + age + ' lata.'
+        'Witaj Drogi Odwiedzający, nazywam się ' + firstName + ' mam ' + age + ' lata.'
     );
 }
 
@@ -33,19 +34,48 @@ greetOld(age, firstName);
 
 const greet = (age, firstName) => {
     console.log(
-        'Witaj Drogi Odwiedzający, nazywam się ' +  firstName + ' i mam ' + age + ' lata.'
+        'Witaj Drogi Odwiedzający, nazywam się ' + firstName + ' i mam ' + age + ' lata.'
     );
 }
 greet(22, 'Maciej');
 
 
-function createContent(querySelectorContent, content){
+function createContent(querySelectorContent, content) {
     const element = document.querySelector(querySelectorContent);
     element.innerHTML = content;
 }
 createContent('.footer--js', 'Strona stwworzona w ramach kursu WTF');
 
-const calculate = (myNumber) => myNumber*7
+const calculate = (myNumber) => myNumber * 7
 console.log(calculate(7));
 
 console.log(console);
+
+const button = document.querySelector('.action--js');
+console.log(button);
+
+const myClick = () => {
+    // console.log('Kliknąłeś');
+    const header = document.querySelector('.main-header--js');
+    header.innerHTML = 'Witaj w moim dzienniczku!';
+}
+
+
+// button.addEventListener('click', myClick);
+
+
+// button.addEventListener('click', (e) => {
+//     console.log(e.target);
+// });
+
+
+button.addEventListener('click', (e) => {
+    const header = document.querySelector('.main-header--js');
+    header.innerHTML = 'Witaj w moim dzienniczku!';
+});
+
+// zamiast 'click' może być 'mouseenter' lub 'mouseleave'!!!
+// button.addEventListener('mouseleave', (e) => {
+//     const header = document.querySelector('.main-header--js');
+//     header.innerHTML = 'Witaj w moim dzienniczku!';
+// });
